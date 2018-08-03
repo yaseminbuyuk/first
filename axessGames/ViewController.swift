1cd6fce97ecd504a88bc49a3966b0146beee982e
Emre YILMAZ <emre.yilmaz@dk.akbank.local>
2018-08-03 09:52:30 +0300

    Initial Commit

---
+++ axessGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  axessGames
+//
+//  Created by Emre YILMAZ on 3.08.2018.
+//  Copyright © 2018 Emre YILMAZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
